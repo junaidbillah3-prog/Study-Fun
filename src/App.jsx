1693dbae499858016946a02bbcd76239b449e9f8
junaidbillah3-prog <junaidbillah3@gmail.com>
2026-08-15 17:29:08 +0200

    Fix leaderboard route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Dashboard from './pages/Dashboard';
 import Quiz from './pages/Quiz';
 import Leaderboard from './pages/Leaderboard';
 
-
-<Route path="/leaderboard" element={<Leaderboard />} />
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/quiz/:subjectId" element={<Quiz />} />
       </Routes>
